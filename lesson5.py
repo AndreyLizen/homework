d87402f1a1abc02c268120bd684cc7bc59949bd7
Andrey Lizenberg <lizenbergaa@mail.ru>
2020-03-02 22:45:52 +0300

    Урок 5

---
+++ lesson5.py
@@ -0,0 +1,44 @@
+#файлы
+# fool = open("test.txt", "w", encoding='utf-8')
+# fool = open("/Users/Andrey/PycharmProjects/lessons/homework/test.txt", "r", encoding='utf-8' )
+
+# content = fool.readlines()
+# print(content)
+
+# for i in fool:
+#     print(i)
+
+# while True:
+#     content = fool.read(1024)
+#     print(content)
+#     if not content:
+#         break
+# fool.close()
+
+# fool = open("/Users/Andrey/PycharmProjects/lessons/homework/test1.txt", "a", encoding='utf-8' )
+# # fool.write("Hello, Pyton!")
+# fool.writelines(["Hello,\n", "Pyton!\n"])
+# fool.close()
+#
+# try:
+#     with open("test1.txt") as fool:
+#         print(type(fool))
+#         for i in fool:
+#             print(i)
+# except IOError:
+#     print("Ошибка!")
+# finally:
+#     print("ого")
+
+# состояния:
+# print("Файл. Имя: ", fool.name)
+# print("Файл. Закрыт: ", fool.closed)
+# print("Файл. Режим: ", fool.mode)
+#
+# import os
+# print(os.listdir())
+
+import json
+data = {"income": {"salary": 50000, "bonus": 20000}}
+with open("my_json", 'w'):
+    json.dump(data, fool)
