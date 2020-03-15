1aae52800448353682a5d42972c7cfa8741e2830
Andrey Lizenberg <lizenbergaa@mail.ru>
2020-03-15 03:02:17 +0300

    + задание 4

--- lesson08/task04.py
+++ lesson08/task04.py
@@ -2,3 +2,39 @@
 # «Оргтехника», который будет базовым для классов-наследников. Эти классы — конкретные типы оргтехники
 # (принтер, сканер, ксерокс). В базовом классе определить параметры, общие для приведенных типов.
 # В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+
+
+class Warehouse:
+    def __init__(self, name, quantity, measure, type_of_eq, in_storage):
+        self.name = name
+        self.quantity = quantity
+        self.measure = measure
+        self.type_of_eq = type_of_eq
+        self.in_storage = in_storage
+
+
+class OfficeEquipment:
+    def __init__(self, name, price, quantity, measure, type_of_eq):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.measure = measure
+        self.type_of_eq = type_of_eq
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, name, price, quantity, measure, is_color):
+        super().__init__(name, price, quantity, measure, type_of_eq='Принтер')
+        self.is_color = is_color
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self, name, price, quantity, measure, type_of_scanning):
+        super().__init__(name, price, quantity, measure, type_of_eq='Сканер')
+        self.type_of_scanning = type_of_scanning
+
+
+class Xerox(OfficeEquipment):
+    def __init__(self, name, price, quantity, measure, is_big):
+        super().__init__(name, price, quantity, measure, type_of_eq='Ксерокс')
+        self.is_big = is_big
